c918517fc86e7384d99a95fc66c5c4e23afa60bb
Egor Tretyakov <TretyakovEDev@yandex.ru>
2026-06-28 23:27:27 +0300

    feat: enable CORS for frontend application

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     })
-  )
+  );
 
   await app.listen(process.env.SERVER_PORT ?? 3000);
 }
